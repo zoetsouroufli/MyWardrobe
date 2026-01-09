1253f9a8c00699e8b90bae66f6c7727378170a16
Μπαμπης Χιωτης <babisheotis@Mpampess-Laptop.local>
2026-01-09 12:38:29 +0200

    Fix web initialization bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
 import 'screens/auth_screen.dart';
@@ -19,7 +20,9 @@ import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if (!kIsWeb) {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
